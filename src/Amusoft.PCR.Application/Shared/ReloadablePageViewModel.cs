64dfb402adda5f9e097d6d6145e9eed17f4c8775
Andreas Müller <taori@users.noreply.github.com>
2023-11-13 22:58:46 +0100

    More logging

--- src/Amusoft.PCR.Application/Shared/ReloadablePageViewModel.cs
+++ src/Amusoft.PCR.Application/Shared/ReloadablePageViewModel.cs
@@ -1,22 +1,36 @@
 ﻿using Amusoft.PCR.Application.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using NLog;
 
 namespace Amusoft.PCR.Application.Shared;
 
 public abstract partial class ReloadablePageViewModel : PageViewModel
 {
+	private static readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
 	private CancellationTokenSource? _reloadCts;
 	[RelayCommand(AllowConcurrentExecutions = false)]
 	protected async Task ReloadAsync()
 	{
+		Log.Debug("Reloading");
+
 		_reloadCts?.Cancel(false);
 		_reloadCts?.Dispose();
 		_reloadCts = new();
-
-		IsReloading = true;
-		await OnReloadAsync(_reloadCts.Token);
-		IsReloading = false;
+		try
+		{
+			IsReloading = true;
+			await OnReloadAsync(_reloadCts.Token);
+		}
+		catch (OperationCanceledException)
+		{
+			// empty on purpose
+		}
+		finally
+		{
+			IsReloading = false;
+		}
 	}
 
 	protected abstract Task OnReloadAsync(CancellationToken cancellationToken);
