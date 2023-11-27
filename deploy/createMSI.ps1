6013ce07d0729ddbe91fb453d3cffb9ed89d1273
Andreas Müller <taori@users.noreply.github.com>
2023-11-27 04:58:08 +0100

    Ci pipeline fix (#14)
    * this fixes #5

--- deploy/createMSI.ps1
+++ deploy/createMSI.ps1
@@ -9,32 +9,39 @@ param (
     [string]$SkipAPK = $false,
     [Parameter(HelpMessage="Whether or not to open explorer afterwards")]
     [string]$OpenExplorer = $false,
-    [Parameter(HelpMessage="Whether or not to open explorer afterwards")]
+    [Parameter(HelpMessage="Product Version for MSI")]
     [string]$ProductVersion = "3.0.0"
 )
 
-Import-Module ".\functions.psm1"
+Import-Module (Resolve-Path "$PSScriptRoot\functions.psm1")
+
+# todo dotnet publish -p:PublishProfile=Package -f net7.0-windows10.0.19041.0 -c Release -o d:\tmp\mauagain -p:PublishDir=D:\tmp\mauiagain2
 
 #Write-Host "Generating MSI with artifacts deletion: $DeleteArtifacts skipPublish: $SkipPublish skipHeat: $SkipHarvesting"
 
 #ütf8
 
 if($DeleteArtifacts -eq $true){
-    Remove-Item -Recurse -Force -Path $(Get-ResolvedPath "$PSScriptRoot\..\artifacts\") -ErrorAction Stop    
+    if(Test-Path "$PSScriptRoot\..\artifacts\"){
+        Remove-Item -Recurse -Force -Path (Resolve-Path "$PSScriptRoot\..\artifacts\") -ErrorAction Stop        
+    }    
 }
 
+$apkFile = Get-ResolvedPath "$PSScriptRoot\..\artifacts\msi\apk\app.apk"
 $installerProject = Get-ResolvedPath "$PSScriptRoot\..\installer\Amusoft.PCR.Installer\Amusoft.PCR.Installer.wixproj"
 $installerOutput = Get-ResolvedPath "$PSScriptRoot\..\artifacts\msi\compiled"
-$apkFile = Get-ResolvedPath "$PSScriptRoot\..\artifacts\msi\apk\app.apk"
 $apkDirectory = [System.IO.Path]::GetDirectoryName($apkFile)
 $solutionDir = Get-ResolvedPath "$PSScriptRoot\..\src\"
 
 $runHeat = "false"
 $runPublish = "false"
+
+Write-Host "Starting creation" -ForegroundColor Cyan
 if($SkipPublish -eq $false){
     $runPublish = "true"
     if($SkipAPK -eq $false){
-        & ".\publishapk.ps1" -PublishFilePath "$apkFile"        
+        Write-Host "Publishing APK" -ForegroundColor Cyan
+        & "$(Get-ResolvedPath "$PSScriptRoot\publishapk.ps1")" -PublishFilePath "$apkFile"        
     }
 }
 
@@ -45,8 +52,8 @@ if($SkipHarvesting -eq $false){
 $buildCode = "dotnet build `"$installerProject`" -c Release -o `"$installerOutput`" -p:SolutionDir=`"$solutionDir`" -p:ApkSource=`"$apkDirectory`" -p:XRunPublish=$runPublish -p:XRunHeat=$runHeat -p:XProductVersion=$ProductVersion -p:XSelfContainedPublish=false"
 Write-Host $buildCode -ForegroundColor Green
 Invoke-Expression $buildCode -ErrorAction Stop
-#&dotnet build "$installerProject" -c Release -o "$installerOutput" -p:SolutionDir=$solutionDir -p:ApkSource=$apkDirectory -p:XRunPublish=`"$runPublish`" -p:XRunHeat=`"$runHeat`"
 
+Write-Host "Renaming MSI" -ForegroundColor Cyan
 Get-ChildItem -Path $installerOutput -Recurse -Filter "*Installer.msi" `
     | %{
         $o = $_.FullName
@@ -55,10 +62,18 @@ Get-ChildItem -Path $installerOutput -Recurse -Filter "*Installer.msi" `
         Move-Item $o $n -Force 
     }   
 
+$msiPath = Get-ChildItem -Path "$installerOutput" -Recurse -Filter "*.msi" `
+            | Where-Object { $_.FullName -like "*en-US*" } `
+            | % { $_.FullName }
+
 if($OpenExplorer -eq $true){
-    Get-ChildItem -Path $installerOutput -Recurse -Filter "*.msi" `
-    | Select-Object -First 1 `
-    | %{ &explorer ([System.IO.Path]::GetDirectoryName($_.FullName)) }    
+    Write-Host "Opening folder for $msiPath" -ForegroundColor Cyan
+    &explorer ([System.IO.Path]::GetDirectoryName($msiPath))
 }
 
 Write-Host "Script complete." -ForegroundColor Green
+
+return @{
+    ApkPath = $apkFile
+    MsiPath = $msiPath
+}
--- deploy/functions.psm1
+++ deploy/functions.psm1
@@ -20,6 +20,11 @@ function Build-Android {
         [ValidateSet('Release','Debug')]
 		[string]$Configuration = "Release"
     )
+    
+    Write-Host "Build-Android: `"KeyStorePath`": $KeyStorePath "`
+                                + " `"ProjectPath`": $ProjectPath "`
+                                + " `"PublishFilePath`": $PublishFilePath "`
+                                + " `"Configuration`": $Configuration "
 
     [Environment]::SetEnvironmentVariable('PCR3PW', $SignPassword, 'Process')
 
@@ -28,6 +33,7 @@ function Build-Android {
     $storePass = "env:PCR3PW"
     
     $tempDest = New-TemporaryDirectory
+    Write-Host "New Temp folder is: $tempDest"
 
     # https://learn.microsoft.com/de-de/dotnet/maui/android/deployment/publish-cli ü
     $publishCode = "dotnet publish $ProjectPath -f net7.0-android -c $Configuration -o `"$tempDest`" -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=$KeyStorePath -p:AndroidSigningKeyAlias=$keyAlias -p:AndroidSigningKeyPass=$keyPass -p:AndroidSigningStorePass=$storePass"
@@ -35,18 +41,24 @@ function Build-Android {
         
     #Invoke-Expression -Command $publishCode -ErrorAction Stop
     &dotnet publish $ProjectPath -f net7.0-android -c $Configuration -o `"$tempDest`" -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=$KeyStorePath -p:AndroidSigningKeyAlias=$keyAlias -p:AndroidSigningKeyPass=$keyPass -p:AndroidSigningStorePass=$storePass | Out-Host
-    
+        
+    Write-Host "Looking for apk file in $tempDest" -ForegroundColor Cyan
     $apk = Get-ChildItem "$tempDest" -Filter "*.apk" | %{$_.FullName}
 
+    Write-Host "Ensure publish directory exists for path $PublishFilePath" -ForegroundColor Cyan
     EnsureDirectoryExists ([System.IO.Path]::GetDirectoryName($PublishFilePath))
+
+    Write-Host "Moving $apk to $PublishFilePath" -ForegroundColor Cyan
     Move-Item $apk $PublishFilePath -Force
+    
+    Write-Host "Removing Temp folder $tempDest" -ForegroundColor Cyan
     Remove-Item -Recurse -Force -Path "$tempDest" -ErrorAction Stop
 }
 
 function New-TemporaryDirectory {
     $parent = [System.IO.Path]::GetTempPath()
     [string] $name = [System.Guid]::NewGuid()
-    $f = New-Item -ItemType Directory -Path (Join-Path $parent $name)
+    $f = New-Item -ItemType Directory -Path (Join-Path "$parent" "$name")
     return $f.FullName
 }
 
