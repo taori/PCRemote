ce652b1e4329e8cb9344549a22bb427bc64355c8
Andreas Müller <taori@users.noreply.github.com>
2023-11-27 15:50:59 +0100

    dead code removal

--- src/Amusoft.PCR.App.UI/Workarounds/GenericTypes.cs
+++
@@ -1,8 +0,0 @@
-﻿using Amusoft.PCR.AM.UI.ViewModels.Shared;
-
-namespace Amusoft.PCR.App.UI.Workarounds;
-
-public static class GenericTypes
-{
-	public static Type NavItemInt => typeof(NavigationItem<int>);
-}
