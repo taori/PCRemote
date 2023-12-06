7fd8d2d13365a6f8dadbccf3df0044dcb2b26d5d
Andreas Müller <taori@users.noreply.github.com>
2023-12-06 11:33:51 +0100

    Warnings for installer if .NET 8 is missing

--- deploy/createMSIFast.ps1
+++ deploy/createMSIFast.ps1
@@ -7,13 +7,28 @@ param (
     [string]$Version="3.0.0",
     
     [Parameter(HelpMessage="Whether or not to open browser at first msi match or not")]
-    [bool]$OpenExplorer=$true
+    [bool]$OpenExplorer=$true,
+    
+    [Parameter(HelpMessage="Start MSI file")]
+    [bool]$OpenMsi=$false
 )
 
 $scriptPath = Resolve-Path "$PSScriptRoot\createMsi.ps1"
 
-switch($Configuration){
-    "Fresh" { & "$scriptPath" -DeleteArtifacts $true -SkipPublish $false -SkipHarvesting $false -OpenExplorer $OpenExplorer -ProductVersion "$Version" }
-    "RecycleBoth" { & "$scriptPath" -DeleteArtifacts $false -SkipPublish $true -SkipHarvesting $true -OpenExplorer $OpenExplorer -ProductVersion "$Version" }
-    "JustHarvest" { & "$scriptPath" -DeleteArtifacts $false -SkipPublish $true -SkipHarvesting $false -OpenExplorer $OpenExplorer -ProductVersion "$Version" }
+if($OpenMsi -eq $true){
+    switch($Configuration){
+        "Fresh" { $r = & "$scriptPath" -DeleteArtifacts $true -SkipPublish $false -SkipHarvesting $false -OpenExplorer $OpenExplorer -ProductVersion "$Version" }
+        "RecycleBoth" { $r = & "$scriptPath" -DeleteArtifacts $false -SkipPublish $true -SkipHarvesting $true -OpenExplorer $OpenExplorer -ProductVersion "$Version" }
+        "JustHarvest" { $r = & "$scriptPath" -DeleteArtifacts $false -SkipPublish $true -SkipHarvesting $false -OpenExplorer $OpenExplorer -ProductVersion "$Version" }
+    }
+
+    & $r.MsiPath    
+}
+else
+{
+    switch($Configuration){
+        "Fresh" { & "$scriptPath" -DeleteArtifacts $true -SkipPublish $false -SkipHarvesting $false -OpenExplorer $OpenExplorer -ProductVersion "$Version" }
+        "RecycleBoth" { & "$scriptPath" -DeleteArtifacts $false -SkipPublish $true -SkipHarvesting $true -OpenExplorer $OpenExplorer -ProductVersion "$Version" }
+        "JustHarvest" { & "$scriptPath" -DeleteArtifacts $false -SkipPublish $true -SkipHarvesting $false -OpenExplorer $OpenExplorer -ProductVersion "$Version" }
+    }    
 }
