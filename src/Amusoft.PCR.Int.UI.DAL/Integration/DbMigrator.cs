5a1373d23899f65938e8db2fca7b8b5e2107a88f
Andreas Müller <taori@users.noreply.github.com>
2024-01-05 19:30:11 +0100

    removal of logging from application startup code (because it depends on the database being setup)

--- src/Amusoft.PCR.Int.UI.DAL/Integration/DbMigrator.cs
+++ src/Amusoft.PCR.Int.UI.DAL/Integration/DbMigrator.cs
@@ -1,27 +1,25 @@
-﻿using Amusoft.PCR.AM.UI.Interfaces;
+﻿using System.Diagnostics;
+using Amusoft.PCR.AM.UI.Interfaces;
 using Amusoft.PCR.Int.UI.DAL.Database;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace Amusoft.PCR.Int.UI.DAL.Integration;
 
 internal class DbMigrator : IApplicationStartup
 {
-	private readonly ILogger<DbMigrator> _log;
 	private readonly UiDbContext _dbContext;
 
-	public DbMigrator(ILogger<DbMigrator> log, UiDbContext dbContext)
+	public DbMigrator(UiDbContext dbContext)
 	{
-		_log = log;
 		_dbContext = dbContext;
 	}
 
 	public void Apply()
 	{
-		_log.LogDebug("Migrating database");
+		Debug.WriteLine("Applying migrations");
 		_dbContext.Database.Migrate();
 		var canConnect = _dbContext.Database.CanConnect();
 		if (!canConnect)
-			_log.LogError("Database unable to connect");
+			Debug.WriteLine("Database unable to connect");
 	}
 }
